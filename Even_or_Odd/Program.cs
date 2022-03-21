f1d7d77834a3b92fe1acc3a5c8349a56d6892850
Sarthak <sarthakpoul@gmail.com>
2022-03-21 21:17:05 +0530

    added Even_or_odd

---
+++ Even_or_Odd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day5FunctionalProblem
+{
+    internal class Program
+    {
+        public static void EvenOdd()
+        {
+            Console.WriteLine("Enter a number to check even or odd: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The given number " + num + " Is even");
+            }
+            else
+            {
+                Console.WriteLine("The given number " + num + " Is Odd");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Program.EvenOdd();
+        }
+    }
+}
